572ae64cfe41bf758e301544353f1b1b9ee769ca
stefanodangelo <61756862+stefanodangelo@users.noreply.github.com>
2023-05-19 22:36:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,18 @@ const GPT_MODE = process.env.GPT_MODE
 let prompt = "You are a helpful Twitch Chatbot."
 
 const messages = [
-  {role: "system", content: "You are a helpful Twitch Chatbot."}
+  {role: "system", content: prompt}
 ];
+
+const user_prompts = [
+];
+
+const bot_answers = [
+];
+
+if (!["LIMITED", "FULL", "PROMPT"].includes(GPT_MODE)) {
+    throw new Error('Unknown parameter GPT_MODE. Please, use one of the following:\nLIMITED\nFULL\nPROMPT\n')
+}
 
 console.log("GPT_MODE is " + GPT_MODE)
 console.log("History length is " + process.env.HISTORY_LENGTH)
@@ -22,31 +32,68 @@ app.all('/', (req, res) => {
     res.send('Yo!')
 })
 
-if (process.env.GPT_MODE === "CHAT"){
 
-  fs.readFile("./file_context.txt", 'utf8', function(err, data) {
-    if (err) throw err;
-    console.log("Reading context file and adding it as system level message for the agent.")
-    messages[0].content = data;
-    console.log(messages[0].content);
-  });
-
-} else {
-
-  fs.readFile("./file_context.txt", 'utf8', function(err, data) {
-    if (err) throw err;
-    console.log("Reading context file and adding it in front of user prompts:")
-    prompt = data;
-    console.log(prompt);
-  });
-
+function load_context() {
+    const file = "./file_context.txt"
+    const encoding = 'utf-8'
+    
+    fs.readFile(file, encoding, function(err, data) {
+        if (err) throw err;
+        console.log("Reading context file and adding it as system level message for the agent.")
+        prompt = data;
+        console.log(prompt);
+    });
+    messages[0].content = prompt;
 }
 
-app.get('/gpt/:text', async (req, res) => {
+
+function count_user_messages(){
+    let user_messages = 0;
+
+    messages.forEach((m) => {
+        if (m.role === 'user') {
+            ++user_messages;
+        }
+    });
     
-    //The agent should recieve Username:Message in the text to identify conversations with different users in his history. 
+    return user_messages
+}
+
+
+function send_answer(response, res){
+    if (response.data.choices) {
+        if(GPT_MODE === "PROMPT"){
+            let agent_response = response.data.choices[0].text
+        } else{
+            let agent_response = response.data.choices[0].message.content
+        }
+        
+        console.log("Agent answer: " + agent_response)
+        messages.push({role: "assistant", content: agent_response})
+        bot_answers.push(agent_response)
+        //prompt = prompt + agent_response;
+        
+        //Check for Twitch max. chat message length limit and slice if needed
+        if(agent_response.length > 399){
+            console.log("Agent answer exceeds twitch chat limit. Slicing to first 399 characters.")
+            agent_response = agent_response.substring(0, 399)
+            console.log("Sliced agent answer: " + agent_response)
+        }
+
+        res.send(agent_response)
+    } else {
+        res.send("Something went wrong. Try again later!")
+    }
+}
+
+
+app.get('/gpt/user:text', async (req, res) => {
+    
+    //The agent should receive Username:Message in the text to identify conversations with different users in his history. 
     
     const text = req.params.text
+    console.log(req.params)
+    console.log(text)
     //const user = req.params.user
     const { Configuration, OpenAIApi } = require("openai");
 
@@ -56,84 +103,52 @@ app.get('/gpt/:text', async (req, res) => {
 
     const openai = new OpenAIApi(configuration);      
     
-    if (GPT_MODE === "CHAT"){
-      //CHAT MODE EXECUTION
-
-      //Add user message to  messages
-      //messages.push({role: "user", content: user+":"+text})
-      messages.push({role: "user", content: text})
-      //Check if message history is exceeded
-      console.log("Conversations in History: " + ((messages.length / 2) -1) + "/" + process.env.HISTORY_LENGTH)
-      if(messages.length > ((process.env.HISTORY_LENGTH * 2) + 1)) {
-          console.log('Message amount in history exceeded. Removing oldest user and agent messages.')
-          messages.splice(1,2)
-     }
-    
-      console.log("Messages: ")
-      console.dir(messages)
-      //console.log("User: ", user)
-      console.log("User Input: " + text)
-
-      const response = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: messages,
-        temperature: 0.5,
-        max_tokens: 128,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-      });
-    
-      if (response.data.choices) {
-        let agent_response = response.data.choices[0].message.content
+    if(user_prompts.includes(text)){
+        res.send(bot_answers[user_prompts.indexOf(text)])
+    } else if (GPT_MODE !== "PROMPT"){
+        //CHAT MODE EXECUTION
+   
+        //Add user message to  messages
+        //messages.push({role: "user", content: user+":"+text})
+        messages.push({role: "user", content: text})
+        user_prompts.push(text)
         
-        console.log("Agent answer: " + agent_response)
-        messages.push({role: "assistant", content: agent_response})
-
-        //Check for Twitch max. chat message length limit and slice if needed
-        if(agent_response.length > 399){
-          console.log("Agent answer exceeds twitch chat limit. Slicing to first 399 characters.")
-          agent_response = agent_response.substring(0, 399)
-          console.log("Sliced agent answer: " + agent_response)
+        if (GPT_MODE === "LIMITED") {
+            //Check if message history is exceeded
+            if (count_user_messages > process.env.HISTORY_LENGTH){
+                console.log('Message amount in history exceeded. Removing oldest   user and agent messages.')
+                messages.splice(1,2)
+                user_prompts.splice(1,2)
+                bot_answers.splice(1,2)
+          }
         }
-
-        res.send(agent_response)
-      } else {
-        res.send("Something went wrong. Try again later!")
-      }
-
+      
+        const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: messages,
+            temperature: 0.5,
+            max_tokens: 128,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+        });
     } else {
-      //PROMPT MODE EXECUTION
-      prompt = prompt + "\n\nQ:" + text + "\nA:";
-      console.log("User Input: " + text)
+        //PROMPT MODE EXECUTION
+        prompt = prompt + "\n\nQ:" + text + "\nA:";
 
-      const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: prompt,
-        temperature: 0.5,
-        max_tokens: 128,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-      });
-      if (response.data.choices) {
-        let agent_response = response.data.choices[0].text
-        console.log("Agent answer: " + agent_response)
-
-        //Check for Twitch max. chat message length limit and slice if needed
-        if(agent_response.length > 399){
-          console.log("Agent answer exceeds twitch chat limit. Slicing to first 399 characters.")
-          agent_response = agent_response.substring(0, 399)
-          console.log ("Sliced Agent answer: " + agent_response)
-        }
-
-        res.send(agent_response)
-        prompt = prompt + agent_response;
-        console.log(prompt)
-      } else {
-          res.send("Something went wrong. Try again later!")
-      }
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: prompt,
+            temperature: 0.5,
+            max_tokens: 128,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+        });
     }
     
+    send_answer(response, res)
 })
+
+
 app.listen(process.env.PORT || 3000)
